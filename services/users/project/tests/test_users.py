42108a2b1af29d2903de1b5c61eda469165b37c7
Michael Gatward <michaelgatward7outlook.com>
2019-09-16 12:15:29 +0100

    Git travis attempt 2

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -71,31 +71,6 @@ class TestUserService(BaseTestCase):
             self.assertIn('Invalid payload.', data['message'])
             self.assertIn('fail', data['status'])
 
-    def test_add_user_duplicate_email(self):
-        """Ensure error is thrown if the email already exists."""
-        with self.client:
-            self.client.post(
-                '/users',
-                data=json.dumps({
-                    'username': 'michael',
-                    'email': 'michael@mherman.org'
-                }),
-                content_type='application/json',
-            )
-            response = self.client.post(
-                '/users',
-                data=json.dumps({
-                    'username': 'michael',
-                    'email': 'michael@mherman.org'
-                }),
-                content_type='application/json',
-            )
-            data = json.loads(response.data.decode())
-            self.assertEqual(response.status_code, 400)
-            self.assertIn(
-                'Sorry. That email already exists.', data['message'])
-            self.assertIn('fail', data['status'])
-
     def test_single_user(self):
         """Ensure get single user behaves correctly."""
         user = add_user('michael', 'michael@mherman.org')
