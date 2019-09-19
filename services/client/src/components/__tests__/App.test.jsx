0ecd0eac77109c811d61da97c78fcf097b3c0513
Michael Gatward <michaelgatward7outlook.com>
2019-09-19 11:00:49 +0100

    Swagger added

--- services/client/src/components/__tests__/App.test.jsx
+++ services/client/src/components/__tests__/App.test.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter as Router } from 'react-router-dom';
 
 import App from '../../App';
+
+// new
+beforeAll(() => {
+  global.localStorage = {
+    getItem: () => 'someToken'
+  };
+});
+
+test('App will call componentWillMount when mounted', () => {
+  const onWillMount = jest.fn();
+  App.prototype.componentWillMount = onWillMount;
+  const wrapper = mount(<Router><App/></Router>);
+  expect(onWillMount).toHaveBeenCalledTimes(1)
+});
 
 test('App renders without crashing', () => {
   const wrapper = shallow(<App/>);
 });
-
-//test('About renders a snapshot properly', () => {
- //   const tree = renderer.create(<About/>).toJSON();
- //   expect(tree).toMatchSnapshot();
- // });
