1ca9eba4603978d1ade97e1a076d31ca67a91838
Michael Gatward <michaelgatward7outlook.com>
2019-09-26 15:06:29 +0100

    Added automatic production services push config

--- docker-deploy-prod.sh
+++ docker-deploy-prod.sh
@@ -24,28 +24,44 @@ then
       fi
     }
 
+    # new
+    update_service() {
+      if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
+        echo "Error updating service."
+        return 1
+      fi
+    }
+
     deploy_cluster() {
 
+      cluster="test-driven-production-cluster" # new
+
       # users
+      service="testdriven-users-prod-service"  # new
       template="ecs_users_prod_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_RDS_URI $PRODUCTION_SECRET_KEY)
       echo "$task_def"
       register_definition
+      update_service  # new
 
       # client
+      service="testdriven-client-prod-service"  # new
       template="ecs_client_prod_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "$task_def"
       register_definition
+      update_service  # new
 
       # swagger
+      service="testdriven-swagger-prod-service"  # new
       template="ecs_swagger_prod_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "$task_def"
       register_definition
+      update_service  # new
 
     }
 
